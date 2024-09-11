695d91f8374af59d082de83fa6b4841b935ab3e0
Jenya Shvartsman <jenyas@1touch.io>
2024-09-11 15:46:29 -0400

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+</script>
+
+<main class="responsive">notes</main>
+
+<footer>
+  <nav>
+    <button class="circle transparent">
+      <i>check_box</i>
+    </button>
+    <button class="circle transparent">
+      <i>image</i>
+    </button>
+
+    <div class="max"></div>
+
+    <button class="square round extra">
+      <i>add</i>
+    </button>
+  </nav>
+</footer>
+
+<style>
+</style>
