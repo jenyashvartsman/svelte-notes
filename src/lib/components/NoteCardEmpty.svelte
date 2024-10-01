f18f0c844640d3a47a87a2dcad1865bdeee56370
Jenya Shvartsman <jenyas@1touch.io>
2024-10-01 15:45:58 -0400

    empty state

---
+++ src/lib/components/NoteCardEmpty.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<article class="medium middle-align center-align">
+  <div>
+    <i class="extra">list_alt</i>
+    <h5>You have no notes</h5>
+    <p>Start by creating new ones</p>
+  </div>
+</article>
--- src/lib/components/NotesGrid.svelte
+++ src/lib/components/NotesGrid.svelte
@@ -3,16 +3,21 @@
   import type { NoteListDto } from "../dtos/note-list.dto";
   import type { NoteTextDto } from "../dtos/note-text.dto";
   import NoteCard from "./NoteCard.svelte";
+  import NoteCardEmpty from "./NoteCardEmpty.svelte";
 
   export let notes: (NoteTextDto | NoteImageDto | NoteListDto)[] = [];
 </script>
 
-<div class="grid large-space notes">
-  {#each notes as note}
-    <div class="s12 m6 l4 notes__item">
-      <NoteCard {note} />
-    </div>
-  {/each}
-</div>
+{#if notes.length}
+  <div class="grid large-space notes">
+    {#each notes as note}
+      <div class="s12 m6 l4 notes__item">
+        <NoteCard {note} />
+      </div>
+    {/each}
+  </div>
+{:else}
+  <NoteCardEmpty />
+{/if}
 
 <style></style>
